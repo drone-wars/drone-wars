4ad8dcf527f3b8eb514d6995c581e12268b76de5
Alastair Lockie <alastair@brandwatch.com>
2014-09-09 16:53:39 +0100

    Tweaked upload behaviour
    - Added templating using hogan
    - Added uploads and success html pages (unstyled)
    - Slugify robot ids
    - Use original extension (png/gif/jpg/whatevs)
    - Existence check on empty file fields

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
+'use strict';
+
 var express = require('express');
 var busboy = require('connect-busboy');
+var hoganHelper = require('./lib/hoganHelper');
 var uploadRobot = require('./middleware/uploadRobot');
 var getRobotIds = require('./middleware/getRobotIds');
 
@@ -8,7 +11,17 @@ var app = express();
 app.use(express.static('frontend', { 'index': ['index.html'] }));
 app.use(busboy());
 
+app.set('view engine', 'template');
+app.set('views', __dirname +  '/views');
+app.engine('template', hoganHelper);
+
+hoganHelper.configure(app);
+
 app.post('/upload-robot', uploadRobot);
 app.get('/robot-ids', getRobotIds);
+app.get('/upload', function(req, res){
+    res.render('uploads.template');
+});
 
 app.listen(3000);
+console.log("Listening on port 3000");
--- middleware/uploadRobot.js
+++ middleware/uploadRobot.js
@@ -1,31 +1,36 @@
 var fs = require('fs');
 var path = require('path');
+var string = require('string');
 
 function checkRobotFolder(subPath, callback) {
   'use strict';
 
   fs.stat(subPath, function (err, stats) {
     if (stats && stats.isDirectory()) {
-      return callback();
+      return callback(null, true);
     }
 
-    fs.mkdir(subPath, callback);
+    fs.mkdir(subPath, function(err){
+      callback(err, false);
+    });
   });
 }
 
-function handleFile(subPath, fieldname, file) {
+function handleFile(subPath, name, file, originalFilename) {
   'use strict';
 
+  var extension = path.extname(originalFilename);
+
   // Direct the possible files to particular file names.
-  switch (fieldname) {
+  switch (name) {
   case 'src':
     return file.pipe(fs.createWriteStream(path.join(subPath, 'src.js')));
 
   case 'body':
-    return file.pipe(fs.createWriteStream(path.join(subPath, 'body.png')));
+    return file.pipe(fs.createWriteStream(path.join(subPath, 'body' + extension)));
 
   case 'turret':
-    return file.pipe(fs.createWriteStream(path.join(subPath, 'turret.js')));
+    return file.pipe(fs.createWriteStream(path.join(subPath, 'turret' + extension)));
 
   default:
     return file.resume();
@@ -35,40 +40,60 @@ function handleFile(subPath, fieldname, file) {
 function uploadRobot(req, res) {
   'use strict';
 
+  var robotId;
   var subPath;
-  var files = {};
+  var pendingFiles = [];
   var gotPath;
+  var isNew = true;
 
   req.busboy.on('field', function (key, value) {
     if (key === 'robot-id') {
-      subPath = path.join(__dirname, '..', 'uploads', value);
+      robotId = string(value).slugify().s;
+      subPath = path.join(__dirname, '..', 'uploads', robotId);
 
-      checkRobotFolder(subPath, function (err) {
+      checkRobotFolder(subPath, function (err, overwritten) {
         if (err) {
+          console.error(err);
           return res.status(500).end();
+        }
+
+        if(overwritten){
+          isNew = false;
         }
 
         gotPath = true;
 
         // Process any stashed files.
-        Object.keys(files).forEach(function (fieldname) {
-          handleFile(subPath, fieldname, files[fieldname]);
+        pendingFiles.forEach(function (pendingFile) {
+          handleFile(subPath, pendingFile.name, pendingFile.file, pendingFile.originalFilename);
         });
       });
     }
   });
 
-  req.busboy.on('file', function (fieldname, file) {
+  req.busboy.on('file', function (fieldname, file, filename) {
+    // Return early if no file specified
+    if(!filename){
+      return file.resume();
+    }
+
     if (gotPath) {
-      return handleFile(subPath, fieldname, file);
+      return handleFile(subPath, fieldname, file, filename);
     }
 
     // If the path isn't sorted yet, stash this.
-    files[fieldname] = file;
+    pendingFiles.push({
+      name: fieldname,
+      file: file,
+      originalFilename: filename
+    });
   });
 
   req.busboy.on('finish', function () {
-    res.status(204).end();
+    res.render('uploadSuccess.template', {
+      name: robotId,
+      verb: isNew ? 'added' : 'updated'
+    });
   });
 
   req.pipe(req.busboy);
