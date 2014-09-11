0121782d637a2d6a7df51ce382fc8c70b88a6007
Alastair Lockie <alastair@brandwatch.com>
2014-09-11 12:25:41 +0100

    added random sprite generator

--- middleware/uploadRobot.js
+++ middleware/uploadRobot.js
@@ -1,10 +1,10 @@
+'use strict';
+
 var fs = require('fs');
 var path = require('path');
 var string = require('string');
 
 function checkRobotFolder(subPath, callback) {
-  'use strict';
-
   fs.stat(subPath, function (err, stats) {
     if (stats && stats.isDirectory()) {
       return callback(null, true);
@@ -17,8 +17,6 @@ function checkRobotFolder(subPath, callback) {
 }
 
 function handleFile(subPath, name, file, originalFilename) {
-  'use strict';
-
   var extension = path.extname(originalFilename);
 
   // Direct the possible files to particular file names.
@@ -37,18 +35,36 @@ function handleFile(subPath, name, file, originalFilename) {
   }
 }
 
-function uploadRobot(req, res) {
-  'use strict';
+// http://stackoverflow.com/a/20272545
+function decodeBase64Image(dataString) {
+  var matches = dataString.match(/^data:([A-Za-z-+\/]+);base64,(.+)$/),
+    response = {};
 
+  if (matches.length !== 3) {
+    return new Error('Invalid data string');
+  }
+
+  return new Buffer(matches[2], 'base64');
+}
+
+function handleBase64(subPath, name, base64) {
+  fs.writeFile(path.join(subPath, name), decodeBase64Image(base64), function(err){
+    if(err){
+        console.error('Error saving Base64', subPath, name, err);
+    }
+  });
+}
+
+function uploadRobot(req, res) {
   var robotId;
   var subPath;
   var pendingFiles = [];
+  var pendingBase64s = {};
   var gotPath;
   var isNew = true;
 
-  req.busboy.on('field', function (key, value) {
-    if (key === 'robot-id') {
-      robotId = string(value).slugify().s;
+  function initalizeRobotDir(fieldValue){
+      robotId = string(fieldValue).slugify().s;
       subPath = path.join(__dirname, '..', 'uploads', robotId);
 
       checkRobotFolder(subPath, function (err, overwritten) {
@@ -67,7 +83,29 @@ function uploadRobot(req, res) {
         pendingFiles.forEach(function (pendingFile) {
           handleFile(subPath, pendingFile.name, pendingFile.file, pendingFile.originalFilename);
         });
+
+        // Process any stashed base 64 images.
+        Object.keys(pendingBase64s).forEach(function (name) {
+          handleBase64(subPath, name, pendingBase64s[name]);
+        });
       });
+  }
+
+  function onBase64Uploaded(name, base64){
+      if(gotPath){
+        handleBase64(subPath, name, base64);
+      } else {
+        pendingBase64s[name] = base64;
+      }
+  }
+
+  req.busboy.on('field', function (key, value) {
+    if (key === 'robot-id') {
+      initalizeRobotDir(value);
+    } else if(key === 'generatedBody' && value.length){
+      onBase64Uploaded('body.png', value);
+    } else if(key === 'generatedTurret' && value.length){
+      onBase64Uploaded('turret.png', value);
     }
   });
 
