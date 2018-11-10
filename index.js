137bbcf963505ea68af2efa1b10e6c0689b89701
Mark S. Everitt <mark.s.everitt@gmail.com>
2018-11-10 18:39:16 +0000

    Ditch template rendering and busboy for simpler files serving and form handling.

--- index.js
+++ index.js
@@ -2,8 +2,7 @@
 
 const path = require('path');
 const express = require('express');
-const busboy = require('connect-busboy');
-const hoganHelper = require('./lib/hoganHelper');
+const formidable = require('express-formidable');
 const uploadRobot = require('./middleware/uploadRobot');
 const getRobotIds = require('./middleware/getRobotIds');
 const getRobotsData = require('./middleware/getRobotsData');
@@ -13,19 +12,8 @@ const app = express();
 app.use(express.static('frontend'));
 app.use(express.static(path.join(__dirname, 'uploads')));
 
-app.use(busboy());
-
-app.set('view engine', 'template');
-app.set('views', path.join(__dirname, '/views'));
-app.engine('template', hoganHelper.render);
-
-hoganHelper.configure(app);
-
-app.get('/', getRobotsData);
-app.post('/upload-robot', uploadRobot);
+app.get('/robots-data', getRobotsData);
+app.post('/upload-robot', formidable(), uploadRobot);
 app.get('/robot-ids', getRobotIds);
-app.get('/upload', (req, res) => {
-  res.render('uploads.template');
-});
 
 app.listen(8080, () => console.log('Listening on port:', 8080)); // eslint-disable-line no-console
