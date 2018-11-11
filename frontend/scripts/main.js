8de4f3a1ac5c09d043ef8cd95c788467877260af
Mark S. Everitt <mark.s.everitt@gmail.com>
2018-11-11 18:22:16 +0000

    Moves frontend entry code to main module.

--- frontend/scripts/main.js
+++ frontend/scripts/main.js
@@ -1,3 +1,26 @@
 import battle from './battle.js';
 
-window.battle = battle;
+const query = new URLSearchParams(document.location.search);
+
+const numAggressors = parseInt(query.get('num-aggressors'), 10) || 0;
+const numAvoiders = parseInt(query.get('num-avoiders'), 10) || 0;
+const numWanderers = parseInt(query.get('num-wanderers'), 10) || 0;
+
+async function start() {
+  const res = await fetch('/robots-data');
+
+  if (!res.ok) {
+    throw new Error(`Unexpected status: ${res.status}`);
+  }
+
+  const { robots } = await res.json();
+
+  battle({
+    customRobots: robots,
+    numAggressors,
+    numAvoiders,
+    numWanderers
+  });
+}
+
+start();
